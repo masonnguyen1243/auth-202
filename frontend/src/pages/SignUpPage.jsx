3f94b55ba1d8677d7c1f2308aabb74b8b170d2e7
masonnguyen1243 <meowjcisgod@gmail.com>
2025-04-29 14:46:57 +0700

    SignUp Page UI

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,93 @@
+import { motion } from "framer-motion";
+import Input from "../components/Input";
+import { Loader, Lock, Mail, User } from "lucide-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+// import { useAuthStore } from "../store/authStore";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // const { signup, error, isLoading } = useAuthStore();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    console.log("Sign up");
+
+    // try {
+    //   await signup(email, password, name);
+    //   navigate("/verify-email");
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-full max-w-md overflow-hidden rounded-2xl bg-gray-800 bg-opacity-50 shadow-xl backdrop-blur-xl backdrop-filter"
+    >
+      <div className="p-8">
+        <h2 className="mb-6 bg-gradient-to-r from-green-400 to-emerald-500 bg-clip-text text-center text-3xl font-bold text-transparent">
+          Create Account
+        </h2>
+
+        <form onSubmit={handleSignUp}>
+          <Input
+            icon={User}
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {/* {error && <p className="mt-2 font-semibold text-red-500">{error}</p>} */}
+          <PasswordStrengthMeter password={password} />
+
+          <motion.button
+            className="mt-5 w-full rounded-lg bg-gradient-to-r from-green-500 to-emerald-600 px-4 py-3 font-bold text-white shadow-lg transition duration-200 hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+            // disabled={isLoading}
+          >
+            {/* {isLoading ? (
+              <Loader className="mx-auto animate-spin" size={24} />
+            ) : (
+              "Sign Up"
+            )} */}
+            Sign Up
+          </motion.button>
+        </form>
+      </div>
+      <div className="flex justify-center bg-gray-900 bg-opacity-50 px-8 py-4">
+        <p className="text-sm text-gray-400">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </motion.div>
+  );
 };
 export default SignUpPage;
